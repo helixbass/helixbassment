3fc58bdfd8184ef0e14498a3ed5a8704a622867f
Julian Rosse <julian@helixbass.net>
2020-07-12 13:50:16 -0400

    leaves

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,42 +1,10 @@
 import React from 'react'
 import {flowMax, addDisplayName} from 'ad-hok'
-import {artNouveauFontStyles} from 'utils/typography'
 import colors from 'utils/colors'
 import {Global, css} from '@emotion/core'
 
 import '../reset.css'
-
-const HeaderBottomBorder = flowMax(addDisplayName('HeaderBottomBorder'), () => (
-  <svg
-    width="100%"
-    height="30px"
-    viewBox="0 -5 500 90"
-    preserveAspectRatio="none"
-    css={styles.headerBottomBorderSvg}
-  >
-    <path
-      d="M249.6,43.2c2.9,15.7-2.2,30.5-9.8,39.2c-17.8,20.3-48.7,19.3-58,18.8c-66.7-3.2-91.5-64.7-147.1-58.8 C20.1,43.9,8.8,49.5,2.8,53c2.7-13.7,2.4-27.6,5.1-41.3c5.7,9.4,19.1,9.3,30.1,8C89.5,13.8,140.9,7.9,192.4,2 c13.5,4.2,27.1,8.4,40.6,12.6C236.4,17.5,246.7,27.1,249.6,43.2z"
-      css={styles.headerBottomBorderFill}
-      transform="translate(0, -24)"
-    />
-    <path
-      d="M1,26.7c6-3.5,17.2-9.1,31.9-10.6c55.7-5.9,80.4,55.6,147.1,58.8c9.3,0.5,40.2,1.5,58-18.8 c2.1-2.4,19-22.2,9.8-39.2C241,4.3,222-2.6,207.8,5.5c-12.7,7.2-19.9,25.2-12.3,36.8c6.8,10.2,21.6,9.9,22.9,9.8"
-      css={styles.headerBottomBorderPath}
-    />
-    <path
-      d="M249.6,43.2c2.9,15.7-2.2,30.5-9.8,39.2c-17.8,20.3-48.7,19.3-58,18.8c-66.7-3.2-91.5-64.7-147.1-58.8 C20.1,43.9,8.8,49.5,2.8,53c2.7-13.7,2.4-27.6,5.1-41.3c5.7,9.4,19.1,9.3,30.1,8C89.5,13.8,140.9,7.9,192.4,2 c13.5,4.2,27.1,8.4,40.6,12.6C236.4,17.5,246.7,27.1,249.6,43.2z"
-      css={styles.headerBottomBorderFill}
-      transform="scale(-1, 1) translate(0, -24)"
-      transform-origin="250 0"
-    />
-    <path
-      d="M1,26.7c6-3.5,17.2-9.1,31.9-10.6c55.7-5.9,80.4,55.6,147.1,58.8c9.3,0.5,40.2,1.5,58-18.8 c2.1-2.4,19-22.2,9.8-39.2C241,4.3,222-2.6,207.8,5.5c-12.7,7.2-19.9,25.2-12.3,36.8c6.8,10.2,21.6,9.9,22.9,9.8"
-      css={styles.headerBottomBorderPath}
-      transform="scale(-1, 1)"
-      transform-origin="250 0"
-    />
-  </svg>
-))
+import Header, {HEADER_HEIGHT} from 'components/Header'
 
 const Layout = flowMax(addDisplayName('Layout'), ({children}) => (
   <>
@@ -52,10 +20,7 @@ const Layout = flowMax(addDisplayName('Layout'), ({children}) => (
       `}
     />
     <div>
-      <h1 css={styles.header}>
-        Helixbassment
-        <HeaderBottomBorder />
-      </h1>
+      <Header />
       <div css={styles.pageContainer}>
         <div css={styles.pageInnerContainer}>{children}</div>
       </div>
@@ -65,23 +30,7 @@ const Layout = flowMax(addDisplayName('Layout'), ({children}) => (
 
 export default Layout
 
-const HEADER_HEIGHT = 40
 const styles = {
-  header: {
-    ...artNouveauFontStyles,
-    color: colors.yellowishWhite,
-    backgroundColor: colors.green,
-    fontSize: 39,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    height: HEADER_HEIGHT,
-  },
   pageContainer: {
     paddingTop: HEADER_HEIGHT,
     paddingLeft: 16,
@@ -102,17 +51,7 @@ const styles = {
     borderRightStyle: 'solid',
     backgroundColor: colors.white,
     paddingTop: 30,
-  },
-  headerBottomBorderSvg: {
-    position: 'absolute',
-    bottom: -23,
-  },
-  headerBottomBorderPath: {
-    fill: 'none',
-    stroke: colors.black,
-    strokeWidth: 8,
-  },
-  headerBottomBorderFill: {
-    fill: colors.green,
+    paddingLeft: 8,
+    paddingRight: 8,
   },
 }
