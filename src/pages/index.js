fc78949c0522bf4832c2f58bec11293eeb01aff2
Julian Rosse <julian@helixbass.net>
2020-07-12 11:27:14 -0400

    set up emotion

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,15 @@ import {artNouveauFontStyles} from 'utils/typography'
 import colors from 'utils/colors'
 
 const Home = flowMax(addDisplayName('Home'), () => (
-  <div
-    style={{
-      ...artNouveauFontStyles,
-      color: colors.white,
-      backgroundColor: colors.green,
-    }}
-  >
-    Hello worldz!
-  </div>
+  <div css={styles.header}>Hello worldz!</div>
 ))
 
 export default Home
+
+const styles = {
+  header: {
+    ...artNouveauFontStyles,
+    color: colors.white,
+    backgroundColor: colors.green,
+  },
+}
